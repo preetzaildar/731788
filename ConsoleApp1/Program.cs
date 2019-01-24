aa2e68acaf2c9eef20c4fca838da442dc90c597d
visualstudio <visualstudio@230-PC32>
2019-01-24 17:23:26 -0500

    commit at 5:23

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace 731788
 {
+    public class customer
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public void promote()
+        {
+            var rating = calculaterating();
+            if (rating == 0)
+            { Console.WriteLine("Promoted to level 1"); }
+            else
+            {//commit again
+                Console.WriteLine("Level 2");
+            }
+        }
+        public int calculaterating()
+        {
+            return 0;
+        }
+
+    }
     class Program
+
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var customer = new customer();
+            //var dog_and_cat = new car();
+            //var bowlimng = new car();
+            //Console.WriteLine("How many cars: {0}",vehicle.howmanycars);
+            //book mybook = new book();
+            //mybook.SetTitle("A separate Peace");
+            //Console.WriteLine(mybook.GetTitle());
         }
     }
+    class vehicle
+
+    {
+        public static int howmanycars = 0;
+        public String Color = "Blue";
+
+    }
+    class car : vehicle
+    {
+        public String Color = "Red";
+        public car()
+        {
+            vehicle.howmanycars++;
+            Console.WriteLine("my color is " + Color);
+        }
+
+    }
+    class Bookcollection
+    {
+
+    }
+    class book : Bookcollection
+    {
+        private String Title;
+        public void SetTitle(String aTitle)
+        {
+            this.Title = aTitle;
+
+        }
+        public String GetTitle() { return this.Title; }
+    }
+
 }
+
